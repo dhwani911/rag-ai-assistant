1935f81b80a13709357a2545fd45ee8b14b15a95
Dhwani Patel <dhwanin.patel@gmail.com>
2026-06-10 18:45:24 -0600

    Add ollama service into docker compose for simplied deployment
    Moved Ollama into the Docker Compose stack to eliminate host-to-container networking dependencies.
    This allows the FastAPI application and Ollama to communicate through Docker's internal network,
    improving portability, reproducibility, and deployment consistency across development & production environments.

--- app/llm.py
+++ app/llm.py
@@ -1,9 +1,11 @@
 # This file loads the llm model
+import os
 
 from langchain_community.llms import Ollama
 from app.config import MODEL_NAME
+OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL")
 
 def load_llm():  
 	""" Load local Ollama model """
-	llm = Ollama(model=MODEL_NAME, base_url="http://host.docker.internal:11434")
+	llm = Ollama(model=MODEL_NAME, base_url=OLLAMA_BASE_URL)
 	return llm
